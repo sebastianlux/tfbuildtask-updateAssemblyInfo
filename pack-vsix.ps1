613c384e76b6636e2c787d477f4bbcdc295f93aa
Sebastian Lux <sebastian.lux@live.com>
2016-12-16 14:53:20 +0100

    Add powershell script to create vsix package more or less automatically

---
+++ pack-vsix.ps1
@@ -0,0 +1,22 @@
+﻿Param ([Parameter(Mandatory=$true)][string]$version)
+
+Add-Type -Assembly "System.IO.Compression.FileSystem" ;
+
+$tmpPath = $env:temp + "\UpdateAssemblyInfo\";
+$zipFilePath = $(get-location).Path + "\UpdateAssemblyInfo-" + $version + ".vsix"
+
+if(Test-Path $tmpPath)
+{
+    Remove-Item $tmpPath -recurse
+}
+
+New-Item $tmpPath -type directory
+Copy-Item ($(get-location).Path + "\readme.md") $tmpPath
+Copy-Item ($(get-location).Path + "\UpdateAssemblyInfo\*") $tmpPath -recurse
+
+[System.IO.Compression.ZipFile]::CreateFromDirectory($tmpPath, $zipFilePath) ;
+
+if(Test-Path $tmpPath)
+{
+    Remove-Item $tmpPath -recurse
+}
