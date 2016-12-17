98c3c6a1886695e4e14a0c1870a2f56bd9eaf5fa
Sebastian Lux <sebastian.lux@live.com>
2016-12-17 19:41:17 +0100

    pack-vsix now applies version number to task.json and  extension.vsixmanifest

--- pack-vsix.ps1
+++ pack-vsix.ps1
@@ -1,5 +1,26 @@
 ﻿Param ([Parameter(Mandatory=$true)][string]$version)
 
+# Update extension.vsixmanifest
+$vsixmanifest = Get-Item ($(get-location).Path + "\UpdateAssemblyInfo\extension.vsixmanifest")
+$tmpFile = $vsixmanifest.FullName + ".tmp"
+
+Get-Content $vsixmanifest.FullName -encoding utf8 |
+    %{$_ -replace ('<Identity Language="en-US" Id="UpdateAssemblyInfo" Publisher="SebastianLux" Version=".*" />'), ('<Identity Language="en-US" Id="UpdateAssemblyInfo" Publisher="SebastianLux" Version="' + $version + '" />') } |
+    Set-Content $tmpFile -encoding utf8
+Move-Item $tmpFile $vsixmanifest.FullName -force
+
+# Update task.json
+$splitVersion = $version -split '.', 0, "simplematch"
+$taskdotjson = Get-Item ($(get-location).Path + "\UpdateAssemblyInfo\UpdateAssemblyInfoTask\task.json")
+$tmpFile = $taskdotjson.FullName + ".tmp"
+Get-Content $taskdotjson.FullName -encoding utf8 |
+    %{$_ -replace ('"Major":  .*,'), ('"Major":  ' + $splitVersion[0] + ',')} |
+    %{$_ -replace ('"Minor":  .*,'), ('"Minor":  ' + $splitVersion[1] + ',')} |
+    %{$_ -replace ('"Patch":  .*'), ('"Patch":  ' + $splitVersion[2])} |
+    Set-Content $tmpFile -encoding utf8
+Move-Item $tmpFile $taskdotjson.FullName -force
+
+# Create vsix package
 Add-Type -Assembly "System.IO.Compression.FileSystem" ;
 
 $tmpPath = $env:temp + "\UpdateAssemblyInfo\";
